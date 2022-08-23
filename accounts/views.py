632a9b46d5a5e490e76f261706c7562ead1f795e
tou0919 <toutou_matthew@hotmail.com>
2022-08-23 17:54:39 +0800

    4

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+# from django.contrib import messages, auth
+from django.contrib.auth.models import User
+# from contacts.models import Contact
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logging in !')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
